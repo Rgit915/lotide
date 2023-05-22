1e0066d5deea38300e95992456328d32a0723d98
Rora Alem <rora.goitom@gmail.com>
2023-05-21 22:09:18 -0400

    file added

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+//eqarrays function compares the two arrays
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  //after checking the length then iterating through each element
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+//The assertArraysEqual() function takes 2 arrays as arguments
+// and uses our eqArrays() function to compare them
+function assertArraysEqual(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+}
+const arr1 = [10, 20, 30];
+const arr2 = [10, 20, 30];
+assertArraysEqual(arr1, arr2);
