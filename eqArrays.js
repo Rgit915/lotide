d7066ef921d1f5153e5f9039916b5f44b38a741b
Rora Alem <rora.goitom@gmail.com>
2023-05-21 21:40:13 -0400

    file updated

--- eqArrays.js
+++ eqArrays.js
@@ -16,7 +16,7 @@ const eqArrays = function(arr1, arr2) {
   }
 
   return true;
-}
+};
 // Test cases
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
