93ce99b6d259bf0ad969672173e47ce9af8b1f82
Rora Alem <rora.goitom@gmail.com>
2023-06-08 11:02:44 -0400

    Refactoring head assertEqual function imported

--- head.js
+++ head.js
@@ -1,10 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//Import the assertEqual function
+const assertEqual = require('./assertEqual');
+//Function head Implementation
 const head = function(arr) {
   if (arr.length === 0) {
     return undefined;
