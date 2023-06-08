463a030efbc1b6b7bb3075a910f24524f19473f7
Rora Alem <rora.goitom@gmail.com>
2023-06-08 12:38:37 -0400

    Test cases moved here from head file

---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+///Import asserEqual file
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+//Test Cases
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([9]),9);
+assertEqual(head([]), undefined);
