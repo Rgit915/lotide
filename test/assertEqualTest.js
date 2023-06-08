423fcffcb26179dbc504194717cd2c0ad553b65b
Rora Alem <rora.goitom@gmail.com>
2023-06-08 11:29:34 -0400

    assertEqualTest file created

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+//
+const assertEqual = require('../assertEqual');
+// TEST CODE
+assertEqual("lotide", "lotide");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(5, 15);
+assertEqual(1, 1);
