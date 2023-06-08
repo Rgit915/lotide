beb652eb1ee1b163b403925b01b831e518d60f15
Rora Alem <rora.goitom@gmail.com>
2023-06-08 16:25:42 -0400

    converted headTest file using Mocha&Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,16 @@
-///Import asserEqual file
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
-//Test Cases
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([9]),9);
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 'Hello' for ['5']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello'); 
+  });
+  it("returns undefined for [] empty array", () => {
+    assert.strictEqual(head([]), undefined); 
+  });
+});
