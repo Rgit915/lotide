327eb8e8ff0987e581b435d4f4a41039c5dfccc5
Rora Alem <rora.goitom@gmail.com>
2023-06-08 13:37:03 -0400

    Import modules assertEqual and eqArrays

---
+++ test/eqArraysTest.js
@@ -0,0 +1,8 @@
+// Import assertEqual and eqArrays modules
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+// Test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
