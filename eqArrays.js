fb56696dd8388dda8e48d649eea66cbd5bda7c04
Rora Alem <rora.goitom@gmail.com>
2023-06-08 13:34:32 -0400

    Module export added and test code removed

--- eqArrays.js
+++ eqArrays.js
@@ -1,24 +1,21 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//Function eqArrays implementation
+
 const eqArrays = function(arr1, arr2) {
+  //takes 2 arrays as parameters and compare their length
   if (arr1.length !== arr2.length) {
+    //if length not equal return false
     return false;
   }
+  // If same length, iterate over each element of arr1
   for (let i = 0; i < arr1.length; i++) {
+    //checks if finds any match, if they're not equal return false
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
-
+  // return true if arrays are equal
   return true;
 };
-// Test cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+// Export 
+module.exports = eqArrays;
