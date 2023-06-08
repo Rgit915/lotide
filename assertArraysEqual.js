47621358176a25526446334a97784badc6865c0a
Rora Alem <rora.goitom@gmail.com>
2023-06-08 14:08:34 -0400

    Imported eqArrays function and test cases removed

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,6 @@
-//eqarrays function compares the two arrays
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  //after checking the length then iterating through each element
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
+//Import eqArrays function
+const eqArrays = require("./eqArrays");
 
-  return true;
-};
 //The assertArraysEqual() function takes 2 arrays as arguments
 // and uses our eqArrays() function to compare them
 const assertArraysEqual = function(arr1, arr2) {
@@ -21,6 +10,7 @@ const assertArraysEqual = function(arr1, arr2) {
     console.log(`🔴🔴🔴 Assertion Failed: [${arr1}] !== [${arr2}]`);
   }
 };
-const arr1 = [10, 20, 30];
-const arr2 = [10, 20, 30];
-assertArraysEqual(arr1, arr2);
+
+
+// Export assertArraysEqual function
+module.exports = assertArraysEqual;
