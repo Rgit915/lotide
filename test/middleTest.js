eb7e924868f06293bfdeb058481cc7a2337acada
Rora Alem <rora.goitom@gmail.com>
2023-06-09 06:24:33 -0400

    Refactored using Moacha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,18 @@
-//Import middle and assertArraysEqual modules
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-//Test Cases:
-assertArraysEqual(middle([]), []); // Empty array, expects []
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]); // Odd number of elements, expects [4]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]); // Even number of elements, expects [4, 5]
-assertArraysEqual(middle(["apple", "banana", "cherry", "date"]), ["banana", "cherry"]); // Even number of elements (strings), expects ["banana", "cherry"]
-assertArraysEqual(middle(["apple", "banana", "cherry", "date", "elderberry"]), ["cherry"]); // Odd number of elements (strings), expects ["cherry"]
+describe("#middle", () => {
+  //use assert.deepEqual from chai for comparing arrays
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([]), [] );  
+  });
+  it("returns [2, 3, 4] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]); 
+  });
+  it("returns ['banana', 'cherry'] for ['apple', 'banana', 'cherry', 'date'] array ", () => {
+    assert.deepEqual(middle(["apple", "banana", "cherry", "date"]), ['banana', 'cherry']);
+  });
+  it("returns ['cherry'] for  ['apple', banana', cherry', 'date', 'elderberry']", () => {
+    assert.deepEqual(middle(["apple", "banana", "cherry", "date", "elderberry"]), ["cherry"]);
+  });
+  
+});
