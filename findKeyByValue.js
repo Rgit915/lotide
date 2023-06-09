c2c61a1a3ace190e8a140e6690c5e3578c461c23
Rora Alem <rora.goitom@gmail.com>
2023-06-09 17:56:48 -0400

    test codes removed and function exported

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-//assertEqual function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 // findKeyByValue function implementation
 //This function is dedigned to search for a key on an object where its value matches a given value
@@ -21,17 +13,5 @@ const findKeyByValue = function (myObject, myValue){
   return undefined  //if no key with given value is found return undefined.
 };
 
-//Test Cases
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire",
-  thriller: "Breaking Bad",
-  fantasy: "Game of Thrones"
-};
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire")); // drama
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"); //✅✅✅ Assertion Passed: drama === drama
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); //✅✅✅ Assertion Passed: undefined === undefined
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi"); //✅✅✅ Assertion Passed: sci_fi === sci_fi
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Breaking Bad"), "thriller"); //✅✅✅ Assertion Passed: thriller === thriller
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Stranger Things"), undefined); //✅✅✅ Assertion Passed: undefined === undefined
+//Export findKeyByValue
+module.exports = findKeyByValue
