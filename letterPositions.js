4d3e51e293e7ddbc1cf802b3461464eee3da410c
Rora Alem <rora.goitom@gmail.com>
2023-06-09 18:02:36 -0400

    test codes removed and func exported

--- letterPositions.js
+++ letterPositions.js
@@ -1,27 +1,4 @@
-//assertArraysEqual function
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: [${arr1}] !== [${arr2}]`);
-  }
-};
-
-//EqArrays function
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-//Function implementation
+///Function implementation
 //it'll return all indices in the string
 //Note that indices are Zero-based
 const letterPositions = function(sentence) {
@@ -43,21 +20,5 @@ const letterPositions = function(sentence) {
   return results;  // return the 'results' object containing all the indices of each character
 };
 
-//Test Cases
-const test1 = letterPositions("hello");
-
-console.log(test1); //{ h: [ 0 ], e: [ 1 ], l: [ 2, 3 ], o: [ 4 ] }
-assertArraysEqual(test1["h"],[0]);    // ✅✅✅ Assertion Passed: [0] === [0]
-assertArraysEqual(test1["e"],[1]);    // ✅✅✅ Assertion Passed: [1] === [1]
-assertArraysEqual(test1["l"],[2,3]);  // ✅✅✅ Assertion Passed: [2,3] === [2,3]
-
-//Test case2
-const  test2 = letterPositions("hello world");
-console.log(test2); //{ h: [ 0 ], e: [ 1 ], l: [ 2, 3 , 9], o: [ 4, 7 ], w: [6], r: [8], d:[10] }
-assertArraysEqual(test2["h"],[0]);    // ✅✅✅ Assertion Passed: [0] === [0]
-assertArraysEqual(test2["e"],[1]);    // ✅✅✅ Assertion Passed: [1] === [1]
-assertArraysEqual(test2["l"],[2,3,9]);  // ✅✅✅ Assertion Passed: [2,3,9] === [2,3,9]
-assertArraysEqual(test2["o"],[4,7]);    // ✅✅✅ Assertion Passed: [4,7] === [4,7]
-assertArraysEqual(test2["w"],[6]);    // ✅✅✅ Assertion Passed: [6] === [6]
-assertArraysEqual(test2["r"],[8]);    // ✅✅✅ Assertion Passed: [8] === [8]
-assertArraysEqual(test2["d"],[10]);    // ✅✅✅ Assertion Passed: [10] === [10]
+//Exporting letterPositions function
+module.export = letterPositions
