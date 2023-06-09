8d34fb0d759ada7d0fad3a8eb98dc7de2d10fca9
Rora Alem <rora.goitom@gmail.com>
2023-06-09 15:14:12 -0400

    Refactored using Mocha and Chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,21 @@
-// Import assertEqual and eqArrays modules
-const assertEqual = require('../assertEqual');
+// Import eqArrays module
+
 const eqArrays = require('../eqArrays');
-// Test cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+const assert = require('chai').assert;
+
+describe("#eqArrays", () => {
+  it("should return true for same elements in the same order", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]),true);
+  });
+  it("should return false for same elements in diferent order", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
+  });
+  it("should return true for ['1', '2', '3'] and ['1', '2', '3']", () => {
+    assert.strictEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true); 
+  });
+  it("should return false for ['1', '2', '3'] and ['1,'2', 3] ", () => {
+    assert.strictEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false); 
+  });
+});
+
+
