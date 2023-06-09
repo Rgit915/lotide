7c9b6397ccf9041c2d25754d7d321d9fdd297d24
Rora Alem <rora.goitom@gmail.com>
2023-06-09 15:23:55 -0400

    test codes moved and module export

--- eqObjects.js
+++ eqObjects.js
@@ -1,26 +1,3 @@
-// eqArrays function
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-//assertEqual function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 //function eqObjects implementation
 // it will take in two objects 
 // returns true or false based on a perfect match
@@ -51,17 +28,5 @@ const eqObjects = function(object1, object2){
   return true;
 };
 
-//Test cases
-const shirtObject = {color: "red", size: "medium"};
-const anotherShirtObject = { size:"medium", color:"red"};
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
-
-const longSleeveShirtObject = {size: "medium", color:"red", sleeveLength: "long"};
-assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
-
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), true); // => true
-
-const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false); // => false
+//Exporting eqObjects function
+module.exports = eqObjects;
